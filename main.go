72c904bc2d13cb8b23c6b3f0c5d41adb4e755262
ren-zi-fa <renzifebriandika923@gmail.com>
2026-05-06 19:14:21 +0700

    **

--- main.go
+++ main.go
@@ -1,83 +1,83 @@
 package main
 
-import "github.com/bps-pasaman-barat/bot_tele_bps.git/question"
+import (
+	"log"
+	"os"
+	"time"
 
-// import (
-// 	"log"
-// 	"os"
-// 	"time"
+	"github.com/bps-pasaman-barat/bot_tele_bps.git/handler"
 
-// 	"github.com/bps-pasaman-barat/bot_tele_bps.git/handler"
-// 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
-// 	"github.com/joho/godotenv"
-// )
 
-// var userTimers = map[int64]*time.Timer{}
-// var sessionActive = map[int64]bool{}
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+	"github.com/joho/godotenv"
+)
+
+var userTimers = map[int64]*time.Timer{}
+var sessionActive = map[int64]bool{}
 
 func main() {
 
-	question.Question()
 
-	// godotenv.Load()
 
-	// token := os.Getenv("BOT_TELEGRAM_TOKEN")
+	godotenv.Load()
 
-	// bot, err := tgbotapi.NewBotAPI(token)
-	// if err != nil {
-	// 	log.Fatal(err)
-	// }
+	token := os.Getenv("BOT_TELEGRAM_TOKEN")
 
-	// updateConfig := tgbotapi.NewUpdate(0)
-	// updateConfig.Timeout = 60
-	// commands := tgbotapi.NewSetMyCommands(
+	bot, err := tgbotapi.NewBotAPI(token)
+	if err != nil {
+		log.Fatal(err)
+	}
 
-	// 	tgbotapi.BotCommand{
-	// 		Command:     "menu",
-	// 		Description: "Menampilkan menu utama",
-	// 	},
-	// )
+	updateConfig := tgbotapi.NewUpdate(0)
+	updateConfig.Timeout = 60
+	commands := tgbotapi.NewSetMyCommands(
 
-	// bot.Request(commands)
+		tgbotapi.BotCommand{
+			Command:     "menu",
+			Description: "Menampilkan menu utama",
+		},
+	)
 
-	// updates := bot.GetUpdatesChan(updateConfig)
+	bot.Request(commands)
 
-	// for update := range updates {
+	updates := bot.GetUpdatesChan(updateConfig)
 
-	// 	if update.Message == nil {
-	// 		continue
-	// 	}
-	// 	chatID := update.Message.Chat.ID
+	for update := range updates {
 
-	// 	if !sessionActive[chatID] {
-	// 		if update.Message.Text != "/menu" {
-	// 			msg := tgbotapi.NewMessage(chatID, "Silakan ketik /menu untuk memulai layanan.")
-	// 			bot.Send(msg)
-	// 			continue
-	// 		}
-	// 		sessionActive[chatID] = true
-	// 	}
+		if update.Message == nil {
+			continue
+		}
+		chatID := update.Message.Chat.ID
 
-	// 	msg := handler.HandleMessage(update)
-	// 	bot.Send(msg)
+		if !sessionActive[chatID] {
+			if update.Message.Text != "/menu" {
+				msg := tgbotapi.NewMessage(chatID, "Silakan ketik /menu untuk memulai layanan.")
+				bot.Send(msg)
+				continue
+			}
+			sessionActive[chatID] = true
+		}
 
-	// 	if t, ok := userTimers[chatID]; ok {
-	// 		t.Stop()
-	// 	}
+		msg := handler.HandleMessage(update)
+		bot.Send(msg)
 
-	// 	userTimers[chatID] = time.AfterFunc(5*time.Minute, func() {
+		if t, ok := userTimers[chatID]; ok {
+			t.Stop()
+		}
 
-	// 		thanks := tgbotapi.NewMessage(chatID,
-	// 			`Terima kasih telah menggunakan BOT LAYANAN BPS PASAMAN BARAT! 😊
+		userTimers[chatID] = time.AfterFunc(5*time.Minute, func() {
 
-	// 			Untuk mendapatkan informasi dan update terbaru dari kami, jangan lupa follow Instagram BPS Pasaman Barat:
-	// 			https://www.instagram.com/bps_pasbar`)
+			thanks := tgbotapi.NewMessage(chatID,
+				`Terima kasih telah menggunakan BOT LAYANAN BPS PASAMAN BARAT! 😊
 
-	// 		bot.Send(thanks)
+				Untuk mendapatkan informasi dan update terbaru dari kami, jangan lupa follow Instagram BPS Pasaman Barat:
+				https://www.instagram.com/bps_pasbar`)
 
-	// 		delete(sessionActive, chatID)
-	// 		delete(userTimers, chatID)
-	// 	})
+			bot.Send(thanks)
 
-	// }
+			delete(sessionActive, chatID)
+			delete(userTimers, chatID)
+		})
+
+	}
 }
